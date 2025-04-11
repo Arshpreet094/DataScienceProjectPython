5f8f5a999f689477a272bb85b58e551c00d3b378
Arshpreet094 <150017796+Arshpreet094@users.noreply.github.com>
2025-04-11 15:15:05 +0530

    Final Objectives.py

--- project.py
+++ project.py
@@ -159,5 +159,76 @@ plt.show()
 
 #------------------------------------------------------------------------------------------------------
 
+#Extracting the major insights and Achieving objectives
 
+
+#Objt_1:Maximum Mobile phone usage on the basis of age, city, and gender
+
+age_1=data[(data['Age']<=20)]
+age_2=data[(data['Age']>20) & (data['Age']<=50)]
+age_3=data[(data['Age']>50)]
+print(f"\n\n\nMobile phone user who are under 20 :{(age_1['Age'].count()/data['Age'].count())*100:.2f}%")
+print(f"Mobile phone user who between 20 to 50 :{(age_2['Age'].count()/data['Age'].count())*100:.2f}%")
+print(f"Mobile phone user who are above 50 :{(age_3['Age'].count()/data['Age'].count())*100:.2f}%\n")
+print("Maximum phone users are between 20 and 50 years\n\n")
+
+
+city=data.groupby('Location')['User ID'].count()
+print("Loction wise usage of phones: ")
+print(city)
+print("\n")
+print("City with maximum usage of phone:",city.idxmax(),"\n\n")
+
+
+gender=data.groupby('Gender')['User ID'].count()
+print("Majority of Mobile phone users are: ",gender.idxmax(),"\n\n")
+
+
+#Objt_2:Analyzing the correlation between screen  timing,data usage,primary uses
+
+corr_matrix=data.corr(numeric_only=True)
+print("Correlation between Screen timings and other uses: ")
+print(corr_matrix)
+plt.figure(figsize=(5,3))
+sns.heatmap(corr_matrix,annot=True,cmap='coolwarm',fmt=".2f",linewidth=0.8)
+plt.xticks(fontsize=6,rotation=45)
+plt.yticks(fontsize=8)
+plt.title("Correlation Matrix")
+plt.show()
+
+
+#Objt_3:Understanding the variation usage across different primary usage categories
+p_use=data.groupby('Primary Use')['Screen Time (hrs/day)'].mean()
+print("\n\n",p_use)
+print("\nThe mobile phone is primarily used for: ",p_use.idxmax())
+plt.figure(figsize=(8,6))
+sns.barplot(data=p_use,palette='deep',width=0.5)
+plt.title("Primary Use of Mobile Phones")
+plt.xlabel("Uses")
+plt.ylabel("No. of users")
+plt.show()
+
+
+#Objt_4:Reccomendations for smart phone compaies to optimize their strategies
+os_used=data.groupby('OS')['User ID'].count()
+print("\n\n\nMost Widely used Mobile OS: ",os_used.idxmax())
+plt.figure(figsize=(6,4))
+plt.pie(os_used,labels=['Android','iOS'],autopct='%1.2f%%',colors=['violet','pink'],startangle=140)
+plt.title("Mobile OS Used")
+plt.axis('equal')
+plt.show()
+
+brand=data.groupby('Phone Brand')['User ID'].count()/data['User ID'].count() *100
+print("Mobile phone brands used are (percentage of users): ")
+print(brand)
+print("The most preffered brand is ",brand.idxmax())
+plt.figure(figsize=(8,6))
+sns.barplot(data=brand,palette='Set2',width=0.4)
+plt.title('Phone Brands used in India')
+plt.xticks(rotation=45)
+plt.xlabel('Phone Brands')
+plt.ylabel('No. of users')
+plt.show()
+
+#-------------------------XXXXXXXXXXXXXXXXXXXXXXXXXXXX---------------------------------------
 
